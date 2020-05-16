2f42ed6c3f1377862f0323311529798f8b5e2cbe
Jacob <jacob@yakovliam.com>
2020-05-16 19:14:36 -0400

    Added JavaDoc to configuration implementations

--- src/main/java/com/yakovliam/spaceapi/config/BukkitConfig.java
+++ src/main/java/com/yakovliam/spaceapi/config/BukkitConfig.java
@@ -10,20 +10,51 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.logging.Level;
 
-//TODO javadoc
 public class BukkitConfig implements Config {
 
+    /**
+     * The configuration object
+     */
     private FileConfiguration config;
+
+    /**
+     * The configuration file object
+     */
     private File configFile;
+
+    /**
+     * The directory that the config lives in (usually {Plugin#getDataFolder()}, but can be customized
+     */
     private File directory;
+
+    /**
+     * The name of the config file (usually "config.yml")
+     */
     private String fileName;
+
+    /**
+     * The contextual plugin
+     */
     private Plugin plugin;
 
+    /**
+     * Initializes new config; directory defaults to {Plugin#getDataFolder()}
+     *
+     * @param plugin The contextual plugin
+     * @param fileName The name of the config as it appears in the jar's resources directory
+     */
     public BukkitConfig(Plugin plugin, String fileName) {
         // call other constructor with dataFolder as directory
         this(plugin, plugin.getDataFolder(), fileName);
     }
 
+    /**
+     * Initializes new config
+     *
+     * @param plugin The contextual plugin
+     * @param fileName The name of the config as it appears in the jar's resources directory
+     * @param directory The custom directory to put the target config in (external, usually in the plugins/PluginName folder)
+     */
     public BukkitConfig(Plugin plugin, File directory, String fileName) {
         // init
         this.plugin = plugin;
@@ -39,6 +70,12 @@ public class BukkitConfig implements Config {
         load();
     }
 
+    /**
+     * Implementation of Bukkit's {Plugin#saveResource()} method to accommodate for this custom configuration directory, etc
+     *
+     * @param resourcePath The path of the resource, usually just the name of the configuration file in the jar (e.g. "config.yml")
+     * @param replace Do we want to replace the external config if it exists?
+     */
     public void saveResource(String resourcePath, boolean replace) {
         resourcePath = resourcePath.replace('\\', '/');
         InputStream in = getResource(resourcePath);
@@ -72,6 +109,12 @@ public class BukkitConfig implements Config {
         }
     }
 
+    /**
+     * Implementation of Bukkit's {Plugin#getResource()} method to accommodate for new features present in this class
+     *
+     * @param filename The name of the file (path CAN be included, but not needed) in the jar's resources folder
+     * @return The inputStream linked with the file in the jar's resource folder
+     */
     public InputStream getResource(String filename) {
         if (filename == null) {
             throw new IllegalArgumentException("Filename cannot be null");
@@ -92,20 +135,39 @@ public class BukkitConfig implements Config {
         }
     }
 
+    /**
+     * Gets the configuration object
+     *
+     * @return The configuration object
+     */
     public FileConfiguration getConfig() {
         return this.config;
     }
 
+    /**
+     * Gets the configuration file object
+     *
+     * @return The configuration file object
+     */
     @Override
     public File getConfigFile() {
         return this.configFile;
     }
 
+    /**
+     * Loads the configuration into an accessible object
+     */
     @Override
     public void load() {
         config = YamlConfiguration.loadConfiguration(configFile);
     }
 
+    /**
+     * Sets a value in the configuration, automatically saves
+     *
+     * @param key The path of the value
+     * @param value The value
+     */
     @Override
     public void set(String key, Object value) {
         config.set(key, value);
@@ -117,11 +179,22 @@ public class BukkitConfig implements Config {
         }
     }
 
+    /**
+     * Gets a value from a path
+     *
+     * @param key The path at which the value is
+     * @return The value
+     */
     @Override
     public Object get(String key) {
         return config.get(key);
     }
 
+    /**
+     * Gets the file name
+     *
+     * @return The file name
+     */
     @Override
     public String getFileName() {
         return fileName;
--- src/main/java/com/yakovliam/spaceapi/config/BungeeConfig.java
+++ src/main/java/com/yakovliam/spaceapi/config/BungeeConfig.java
@@ -9,10 +9,27 @@ import java.io.*;
 
 public class BungeeConfig implements Config {
 
+    /**
+     * The configuration object
+     */
     private Configuration config;
+
+    /**
+     * The configuration file where the configuration lives
+     */
     private File configFile;
+
+    /**
+     * The name of the configuration file (usually "config.yml")
+     */
     private String fileName;
 
+    /**
+     * Initialize new config
+     *
+     * @param plugin The contextual plugin; BungeeCord
+     * @param fileName The name of the file as it appears in the jar resources directory
+     */
     public BungeeConfig(Plugin plugin, String fileName) {
         // load file
         configFile = new File(plugin.getDataFolder(), fileName);
@@ -38,15 +55,28 @@ public class BungeeConfig implements Config {
         load();
     }
 
+    /**
+     * Gets the configuration object
+     *
+     * @return The configuration object
+     */
     public Configuration getConfig() {
         return this.config;
     }
 
+    /**
+     * Gets the config file object
+     *
+     * @return The configuration file object
+     */
     @Override
     public File getConfigFile() {
         return this.configFile;
     }
 
+    /**
+     * Loads the configuration into an accessible configuration object
+     */
     @Override
     public void load() {
         try {
@@ -56,6 +86,12 @@ public class BungeeConfig implements Config {
         }
     }
 
+    /**
+     * Sets a value in the configuration, automatically saves
+     *
+     * @param key The path of the value
+     * @param value The value
+     */
     @Override
     public void set(String key, Object value) {
         config.set(key, value);
@@ -68,11 +104,22 @@ public class BungeeConfig implements Config {
         }
     }
 
+    /**
+     * Gets a value from a path
+     *
+     * @param key The path at which the value is
+     * @return The value
+     */
     @Override
     public Object get(String key) {
         return config.get(key);
     }
 
+    /**
+     * Gets the file name
+     *
+     * @return The file name
+     */
     @Override
     public String getFileName() {
         return fileName;
