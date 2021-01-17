7d2f5056ecb85b73efcbc232cf8221fb3fe25c39
yakovliam <jacob@yakovliam.com>
2021-01-17 17:53:52 -0500

    :zap: Update color util

--- src/main/java/dev/spaceseries/api/util/ColorUtil.java
+++ src/main/java/dev/spaceseries/api/util/ColorUtil.java
@@ -14,55 +14,57 @@ public class ColorUtil {
 
     private static final Pattern URL = Pattern.compile("^(?:(https?)://)?([-\\w_\\.]{2,}\\.[a-z]{2,4})(/\\S*)?$");
     public static final Pattern HEX_PATTERN = Pattern.compile("&\\(#([A-Fa-f0-9]{6})\\)");
-    public static final char COLOR_CHAR = ChatColor.COLOR_CHAR;
+    public static final char COLOR_CHAR = '\uA007';
+    private static final Pattern STRIP_COLOR_PATTERN = Pattern.compile("(?i)" + COLOR_CHAR + "[0-9A-FK-ORX]");
 
     /**
-     * Converts the old formatting system that used
-     * {@link ChatColor#COLOR_CHAR} into the new json based
-     * system.
+     * Converts string into baseComponents
      *
-     * @param message the text to convert
-     * @return the components needed to print the message to the client
+     * @param message message
      */
+    @Deprecated
     public static BaseComponent[] fromLegacyText(String message) {
         return fromLegacyText(message, ChatColor.WHITE);
     }
 
     /**
-     * Converts the old formatting system that used
-     * {@link ChatColor#COLOR_CHAR} into the new json based
-     * system.
+     * Converts string into baseComponents
      *
-     * @param message      the text to convert
-     * @param defaultColor color to use when no formatting is to be applied
-     *                     (i.e. after ChatColor.RESET).
-     * @return the components needed to print the message to the client
+     * @param message      message
+     * @param defaultColor the default color
      */
+    @Deprecated
     public static BaseComponent[] fromLegacyText(String message, ChatColor defaultColor) {
-        List<BaseComponent> components = new ArrayList<>();
+        List<BaseComponent> components = new ArrayList();
         StringBuilder builder = new StringBuilder();
         TextComponent component = new TextComponent();
         Matcher matcher = URL.matcher(message);
 
-        for (int i = 0; i < message.length(); i++) {
+        for (int i = 0; i < message.length(); ++i) {
             char c = message.charAt(i);
-            if (c == ChatColor.COLOR_CHAR) {
-                if (++i >= message.length()) {
+            TextComponent old;
+            if (c == 167) {
+                ++i;
+                if (i >= message.length()) {
                     break;
                 }
+
                 c = message.charAt(i);
                 if (c >= 'A' && c <= 'Z') {
-                    c += 32;
+                    c = (char) (c + 32);
                 }
+
                 ChatColor format;
                 if (c == 'x' && i + 12 < message.length()) {
                     StringBuilder hex = new StringBuilder("#");
-                    for (int j = 0; j < 6; j++) {
-                        hex.append(message.charAt(i + 2 + (j * 2)));
+
+                    for (int j = 0; j < 6; ++j) {
+                        hex.append(message.charAt(i + 2 + j * 2));
                     }
+
                     try {
                         format = ChatColor.of(hex.toString());
-                    } catch (IllegalArgumentException ex) {
+                    } catch (IllegalArgumentException var11) {
                         format = null;
                     }
 
@@ -70,72 +72,75 @@ public class ColorUtil {
                 } else {
                     format = ChatColor.getByChar(c);
                 }
-                if (format == null) {
-                    continue;
-                }
-                if (builder.length() > 0) {
-                    TextComponent old = component;
-                    component = new TextComponent(old);
-                    old.setText(builder.toString());
-                    builder = new StringBuilder();
-                    components.add(old);
-                }
-                if (format == ChatColor.BOLD) {
-                    component.setBold(true);
-                } else if (format == ChatColor.ITALIC) {
-                    component.setItalic(true);
-                } else if (format == ChatColor.UNDERLINE) {
-                    component.setUnderlined(true);
-                } else if (format == ChatColor.STRIKETHROUGH) {
-                    component.setStrikethrough(true);
-                } else if (format == ChatColor.MAGIC) {
-                    component.setObfuscated(true);
-                } else if (format == ChatColor.RESET) {
-                    format = defaultColor;
-                    component = new TextComponent();
-                    component.setColor(format);
 
-                    r(component);
+                if (format != null) {
+                    if (builder.length() > 0) {
+                        old = component;
+                        component = new TextComponent(component);
+                        old.setText(builder.toString());
+                        builder = new StringBuilder();
+                        components.add(old);
+                    }
+
+                    if (format == ChatColor.BOLD) {
+                        component.setBold(true);
+                    } else if (format == ChatColor.ITALIC) {
+                        component.setItalic(true);
+                    } else if (format == ChatColor.UNDERLINE) {
+                        component.setUnderlined(true);
+                    } else if (format == ChatColor.STRIKETHROUGH) {
+                        component.setStrikethrough(true);
+                    } else if (format == ChatColor.MAGIC) {
+                        component.setObfuscated(true);
+                    } else if (format == ChatColor.RESET) {
+                        r(component);
+                        component.setColor(defaultColor);
+                        component.setBold(false);
+                    } else {
+                        component = new TextComponent();
+                        component.setColor(format);
+                    }
+                }
+            } else {
+                int pos = message.indexOf(32, i);
+                if (pos == -1) {
+                    pos = message.length();
+                }
+
+                if (matcher.region(i, pos).find()) {
+                    if (builder.length() > 0) {
+                        old = component;
+                        component = new TextComponent(component);
+                        old.setText(builder.toString());
+                        builder = new StringBuilder();
+                        components.add(old);
+                    }
+
+                    old = component;
+                    component = new TextComponent(component);
+                    String urlString = message.substring(i, pos);
+                    component.setText(urlString);
+                    component.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, urlString.startsWith("http") ? urlString : "http://" + urlString));
+                    components.add(component);
+                    i += pos - i - 1;
+                    component = old;
                 } else {
-                    component = new TextComponent();
-                    component.setColor(format);
+                    builder.append(c);
                 }
-                continue;
             }
-            int pos = message.indexOf(' ', i);
-            if (pos == -1) {
-                pos = message.length();
-            }
-            if (matcher.region(i, pos).find()) { //Web link handling
-
-                if (builder.length() > 0) {
-                    TextComponent old = component;
-                    component = new TextComponent(old);
-                    old.setText(builder.toString());
-                    builder = new StringBuilder();
-                    components.add(old);
-                }
-
-                TextComponent old = component;
-                component = new TextComponent(old);
-                String urlString = message.substring(i, pos);
-                component.setText(urlString);
-                component.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,
-                        urlString.startsWith("http") ? urlString : "http://" + urlString));
-                components.add(component);
-                i += pos - i - 1;
-                component = old;
-                continue;
-            }
-            builder.append(c);
         }
 
         component.setText(builder.toString());
         components.add(component);
-
         return components.toArray(new BaseComponent[0]);
     }
 
+    /**
+     * Resets a component
+     *
+     * @param component component
+     */
+    @Deprecated
     private static void r(TextComponent component) {
         component.setItalic(false);
         component.setUnderlined(false);
@@ -145,22 +150,39 @@ public class ColorUtil {
     }
 
     /**
-     * Translates a message's chatColors & hex colors
+     * Translates from ampersand
      *
-     * @param message The message
-     * @return The translated message
+     * @param message message
+     * @return translated
      */
+    @Deprecated
     public static String translateFromAmpersand(String message) {
         try {
             Matcher matcher = HEX_PATTERN.matcher(message);
-            StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
+            StringBuffer buffer = new StringBuffer(message.length() + 32);
+
             while (matcher.find()) {
                 String group = matcher.group(1);
                 matcher.appendReplacement(buffer, ChatColor.of("#" + group).toString());
             }
+
             return ChatColor.translateAlternateColorCodes('&', matcher.appendTail(buffer).toString());
-        } catch (NoSuchMethodError e) {
+        } catch (NoSuchMethodError var4) {
             return ChatColor.translateAlternateColorCodes('&', message);
         }
+    }
+
+    /**
+     * Strips color
+     *
+     * @param input input
+     * @return stripped string
+     */
+    public static String stripColor(final String input) {
+        if (input == null) {
+            return null;
+        }
+
+        return STRIP_COLOR_PATTERN.matcher(input).replaceAll("");
     }
 }
